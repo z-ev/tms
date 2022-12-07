579e144cc2512993273a181fd14c12fbc179bbb7
z-ev <e@zab.bz>
2022-12-07 10:43:17 +0300

    chore(Entities): add has one relation with merchant to user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,7 @@ declare(strict_types=1);
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
@@ -79,4 +80,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * @return HasOne<Merchant>
+     */
+    public function user(): HasOne
+    {
+        return $this->hasOne(Merchant::class, 'id', 'merchant_id');
+    }
 }
