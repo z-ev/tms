620491b9c3c9e5ecc3f1154ff0cfbb1b26e813ae
z-ev <e@zab.bz>
2022-12-09 20:12:03 +0300

    chore: correct user factory and seeder

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -32,8 +32,8 @@ class UserFactory extends Factory
             'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token'    => Str::random(10),
             'merchant_id'       => DatabaseSeeder::MOCK_MERCHANT_ID,
-            'roles'             => json_encode([User::ROLE_ADMIN]),
-            'settings'          => json_encode([User::SETTINGS]),
+            'roles'             => [User::ROLE_ADMIN],
+            'settings'          => [User::SETTINGS],
             'status_id'         => User::STATUS_ACTIVE,
         ];
     }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -22,22 +22,22 @@ class UserSeeder extends Seeder
     {
         User::factory()->create([
             'email'       => 'admin@admin.ru',
-            'roles'       => json_encode([User::ROLE_ADMIN]),
+            'roles'       => [User::ROLE_ADMIN],
             'merchant_id' => DatabaseSeeder::MOCK_MERCHANT_ID,
             'status_id'   => User::STATUS_ACTIVE,
         ]);
         User::factory()->create([
-            'roles'       => json_encode([User::ROLE_MERCHANT]),
+            'roles'       => [User::ROLE_MERCHANT],
             'merchant_id' => DatabaseSeeder::MOCK_MERCHANT_ID,
             'status_id'   => User::STATUS_ACTIVE,
         ]);
         User::factory()->create([
-            'roles'       => json_encode([User::ROLE_OPERATOR]),
+            'roles'       => [User::ROLE_OPERATOR],
             'merchant_id' => DatabaseSeeder::MOCK_MERCHANT_ID,
             'status_id'   => User::STATUS_ACTIVE,
         ]);
         User::factory()->create([
-            'roles'       => json_encode([User::ROLE_CLIENT]),
+            'roles'       => [User::ROLE_CLIENT],
             'merchant_id' => DatabaseSeeder::MOCK_MERCHANT_ID,
             'status_id'   => User::STATUS_ACTIVE,
         ]);
